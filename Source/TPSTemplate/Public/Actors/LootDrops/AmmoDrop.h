2ed30d29275d569c78dcaa8d1418b02a10c7b636
Lewis Wardman <lewis2304@hotmail.co.uk>
2024-05-29 13:00:09 +0100

    Inherited OnOverlapBegin from LootDrop.h

--- Source/TPSTemplate/Public/Actors/LootDrops/AmmoDrop.h
+++ Source/TPSTemplate/Public/Actors/LootDrops/AmmoDrop.h
@@ -9,5 +9,9 @@ UCLASS()
 class TPSTEMPLATE_API AAmmoDrop : public ALoot
 {
 	GENERATED_BODY()
+
+	virtual void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+                        class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                        const FHitResult& SweepResult) override;
 	
 };
