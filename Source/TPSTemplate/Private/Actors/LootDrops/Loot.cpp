9df28f2501b538ee769320df160ec9a2433c22fe
Lewis Wardman <lewis2304@hotmail.co.uk>
2024-05-29 16:33:27 +0100

    included niagara function library
    Locked the mesh's rotation around the z axis
    
    Spawned loot marker in begin play

--- Source/TPSTemplate/Private/Actors/LootDrops/Loot.cpp
+++ Source/TPSTemplate/Private/Actors/LootDrops/Loot.cpp
@@ -2,22 +2,42 @@
 
 #include "Components/BoxComponent.h"
 
+#include "NiagaraFunctionLibrary.h"
+
 ALoot::ALoot()
 {
  	PrimaryActorTick.bCanEverTick = true;
 
 	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
-	RootComponent = Mesh;
+	Mesh->SetSimulatePhysics(true);
+	Mesh->BodyInstance.bLockXRotation = true;
+	Mesh->BodyInstance.bLockYRotation = true;
 
-	BoxCollision = CreateDefaultSubobject<UBoxComponent>(FName("Box Collision"));
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Collision"));
 	BoxCollision->SetupAttachment(Mesh);
-
 	BoxCollision->OnComponentBeginOverlap.AddDynamic(this, &ALoot::OnOverlapBegin);
 }
 
 void ALoot::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (GetWorld() && LootMarker && Mesh)
+	{
+		UNiagaraFunctionLibrary::SpawnSystemAttached
+			( 
+			LootMarker,
+			RootComponent,
+			FName("None"),
+			GetActorLocation(),
+			GetActorRotation(),
+			EAttachLocation::KeepWorldPosition,
+			true, //unsure
+			true, //unsure
+			ENCPoolMethod::None,
+			true //unsure
+			);
+	}
 }
 
 void ALoot::Tick(float DeltaTime)
