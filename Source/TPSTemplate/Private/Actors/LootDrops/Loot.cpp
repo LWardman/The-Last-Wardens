7a932c5a507f83cbb1dcaf475bcaa16f7fea4313
Lewis Wardman <lewis2304@hotmail.co.uk>
2024-05-29 12:59:33 +0100

    Defined OnOverlapBegin and bound it to the box collisions overlap event

--- Source/TPSTemplate/Private/Actors/LootDrops/Loot.cpp
+++ Source/TPSTemplate/Private/Actors/LootDrops/Loot.cpp
@@ -11,6 +11,8 @@ ALoot::ALoot()
 
 	BoxCollision = CreateDefaultSubobject<UBoxComponent>(FName("Box Collision"));
 	BoxCollision->SetupAttachment(Mesh);
+
+	BoxCollision->OnComponentBeginOverlap.AddDynamic(this, &ALoot::OnOverlapBegin);
 }
 
 void ALoot::BeginPlay()
@@ -23,3 +25,9 @@ void ALoot::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 }
 
+void ALoot::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+                        class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                        const FHitResult& SweepResult)
+{
+	
+}
