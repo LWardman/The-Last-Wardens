00bbfbb115b7f9a0c019e15b3e1c6d5ac6fbec6f
Lewis Wardman <lewis2304@hotmail.co.uk>
2024-05-29 13:00:27 +0100

    Defined OnOverlapBegin

--- Source/TPSTemplate/Private/Actors/LootDrops/AmmoDrop.cpp
+++ Source/TPSTemplate/Private/Actors/LootDrops/AmmoDrop.cpp
@@ -1,2 +1,20 @@
 #include "Actors/LootDrops/AmmoDrop.h"
 
+#include "Characters/Shooter.h"
+#include "Actors/Weapon.h"
+
+void AAmmoDrop::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+                        class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                        const FHitResult& SweepResult)
+{
+    Super::OnOverlapBegin(OverlappedComp, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+    if (AShooter* Player = Cast<AShooter>(OtherActor))
+    {
+        if (Player->HasWeaponEquipped() && !Player->Weapon->ReserveIsFull())
+        {
+            Player->Weapon->AddAmmoToReserve(Player->Weapon->ClipSize);
+            Destroy();
+        }
+    }
+}
