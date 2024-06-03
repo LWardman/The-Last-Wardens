ca0313a5519e8c70e309f7df300f1b315f909721
Lewis Wardman <lewis2304@hotmail.co.uk>
2024-06-03 13:38:05 +0100

    overriden OnOverlapBegin

--- Source/TPSTemplate/Private/Actors/LootDrops/ResourceDrop.cpp
+++ Source/TPSTemplate/Private/Actors/LootDrops/ResourceDrop.cpp
@@ -1,2 +1,30 @@
 #include "Actors/LootDrops/ResourceDrop.h"
 
+#include "Characters/TPSTemplateCharacter.h"
+#include "Components/ResourceTracker.h"
+
+void AResourceDrop::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+                        class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                        const FHitResult& SweepResult)
+{
+    Super::OnOverlapBegin(OverlappedComp, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+    if (ATPSTemplateCharacter* Player = Cast<ATPSTemplateCharacter>(OtherActor))
+    {
+        if (UResourceTracker* Tracker = Player->ResourceTracker)
+        {
+            if (bIsCommonResource)
+            {
+                Tracker->IncreaseResourceCounter(Tracker->CommonResourceCounter, ResourceCount);
+            }
+            else if (bIsRareResource)
+            {
+                Tracker->IncreaseResourceCounter(Tracker->RareResourceCounter, ResourceCount);
+            }
+            if (bIsLegendaryResource)
+            {
+                Tracker->IncreaseResourceCounter(Tracker->LegendaryResourceCounter, ResourceCount);
+            }
+        }
+    }
+}
