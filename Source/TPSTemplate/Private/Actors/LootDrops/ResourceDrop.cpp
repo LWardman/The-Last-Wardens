7da590c4f52bbf5990ea6a9be7d70fc01fd60664
Lewis Wardman <lewis2304@hotmail.co.uk>
2024-06-03 12:36:08 +0100

    Initial Commit

---
+++ Source/TPSTemplate/Private/Actors/LootDrops/ResourceDrop.cpp
@@ -0,0 +1,2 @@
+#include "Actors/LootDrops/ResourceDrop.h"
+
---
+++ Source/TPSTemplate/Public/Actors/LootDrops/ResourceDrop.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/LootDrops/Loot.h"
+#include "ResourceDrop.generated.h"
+
+
+UCLASS()
+class TPSTEMPLATE_API AResourceDrop : public ALoot
+{
+	GENERATED_BODY()
+	
+};
