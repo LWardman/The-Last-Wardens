e5df5a30d1c6831934b05ab7df1ee89047650ec4
Lewis Wardman <lewis2304@hotmail.co.uk>
2024-06-03 13:37:49 +0100

    overriden OnOverlapBegin function
    declared bools to say what type of resource it is
    
    declared an int of how much the resource should add to the counter

--- Source/TPSTemplate/Public/Actors/LootDrops/ResourceDrop.h
+++ Source/TPSTemplate/Public/Actors/LootDrops/ResourceDrop.h
@@ -10,4 +10,19 @@ class TPSTEMPLATE_API AResourceDrop : public ALoot
 {
 	GENERATED_BODY()
 	
+    virtual void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+                        class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                        const FHitResult& SweepResult) override;
+
+	UPROPERTY(EditAnywhere, Category = "Resource Stats")
+	bool bIsCommonResource = false;
+
+	UPROPERTY(EditAnywhere, Category = "Resource Stats")
+	bool bIsRareResource = false;
+
+	UPROPERTY(EditAnywhere, Category = "Resource Stats")
+	bool bIsLegendaryResource = false;
+
+	UPROPERTY(EditAnywhere, Category = "Resource Stats")
+	int32 ResourceCount = 0;
 };
