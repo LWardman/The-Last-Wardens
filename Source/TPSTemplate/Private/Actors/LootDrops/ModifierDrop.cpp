d3157348e6887f3816db3b31afb28eb43de9ec22
Lewis Wardman <lewis2304@hotmail.co.uk>
2024-06-05 15:42:35 +0100

    Defined Tick, OnOverlapBegin and RotateModifierDropped, which is called in tick

--- Source/TPSTemplate/Private/Actors/LootDrops/ModifierDrop.cpp
+++ Source/TPSTemplate/Private/Actors/LootDrops/ModifierDrop.cpp
@@ -1,2 +1,24 @@
 #include "Actors/LootDrops/ModifierDrop.h"
 
+
+void AModifierDrop::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+    RotateModifierDrop(DeltaTime);
+}
+
+void AModifierDrop::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+                        class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                        const FHitResult& SweepResult)
+{
+	Super::OnOverlapBegin(OverlappedComp, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+}
+
+void AModifierDrop::RotateModifierDrop(float DeltaTime)
+{
+	FRotator YawRotation = FRotator(0.f, RotationSpeed, 0.f);
+
+	SetActorRotation(GetActorRotation() + DeltaTime * YawRotation);
+}
