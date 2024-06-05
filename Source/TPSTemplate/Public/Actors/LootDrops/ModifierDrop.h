b30e3d82dcc6a5f2b4aa0bb37e9b8c356a107b91
Lewis Wardman <lewis2304@hotmail.co.uk>
2024-06-05 12:09:09 +0100

    Initial Commit

---
+++ Source/TPSTemplate/Public/Actors/LootDrops/ModifierDrop.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/LootDrops/Loot.h"
+#include "ModifierDrop.generated.h"
+
+
+UCLASS()
+class TPSTEMPLATE_API AModifierDrop : public ALoot
+{
+	GENERATED_BODY()
+	
+};
