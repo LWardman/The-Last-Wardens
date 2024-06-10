ec46d4a5938eef611d28bcd6dc9cb041bf12a2b3
Lewis Wardman <lewis2304@hotmail.co.uk>
2024-06-10 13:09:58 +0100

    included shooter, weapon and modifier components
    Defined OnOverlapBegin()

--- Source/TPSTemplate/Private/Actors/LootDrops/DamageBuff.cpp
+++ Source/TPSTemplate/Private/Actors/LootDrops/DamageBuff.cpp
@@ -1,5 +1,26 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Actors/LootDrops/DamageBuff.h"
 
+#include "Characters/Shooter.h"
+#include "Actors/Weapon.h"
+#include "Components/ModifierComponent.h"
+
+void ADamageBuff::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+                        class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                        const FHitResult& SweepResult)
+{
+	Super::OnOverlapBegin(OverlappedComp, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+    if (!OtherActor) return;
+
+    AShooter* Character = Cast<AShooter>(OtherActor);
+    if (!Character) return;
+
+    AWeapon* Weapon = Character->Weapon;
+    if (!Weapon) return;
+
+    if (UModifierComponent* ModComponent = Weapon->DamageModifierComponent)
+    {
+        ModComponent->ApplyBuff(DamageMultiplier, BuffDuration);
+        Destroy();
+    }
+}
