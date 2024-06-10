bde1efe07555062a33e77014c9db94635f579a7c
Lewis Wardman <lewis2304@hotmail.co.uk>
2024-06-10 13:09:32 +0100

    Overriden OnOverlapBegin
    declared damage multiplier and buff duration

--- Source/TPSTemplate/Public/Actors/LootDrops/DamageBuff.h
+++ Source/TPSTemplate/Public/Actors/LootDrops/DamageBuff.h
@@ -1,17 +1,22 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Actors/LootDrops/ModifierDrop.h"
 #include "DamageBuff.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class TPSTEMPLATE_API ADamageBuff : public AModifierDrop
 {
 	GENERATED_BODY()
 	
+public:
+
+	virtual void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+                        class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                        const FHitResult& SweepResult) override;
+
+    float DamageMultiplier = 1.5f;
+
+    float BuffDuration = 30.f;
 };
