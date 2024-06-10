cde116f1d625e49fcac45f9a9cf26916dffdc29f
Lewis Wardman <lewis2304@hotmail.co.uk>
2024-06-10 12:46:53 +0100

    Initial Commit

---
+++ Source/TPSTemplate/Public/Actors/LootDrops/DamageBuff.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/LootDrops/ModifierDrop.h"
+#include "DamageBuff.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPSTEMPLATE_API ADamageBuff : public AModifierDrop
+{
+	GENERATED_BODY()
+	
+};
