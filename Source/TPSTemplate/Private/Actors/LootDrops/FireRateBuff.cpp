fb56838a30a5a5fc0c2f72dc8a68d19045ab3996
Lewis Wardman <lewis2304@hotmail.co.uk>
2024-06-13 15:26:03 +0100

    Initial Commit

---
+++ Source/TPSTemplate/Private/Actors/LootDrops/FireRateBuff.cpp
@@ -0,0 +1,2 @@
+#include "Actors/LootDrops/FireRateBuff.h"
+
---
+++ Source/TPSTemplate/Public/Actors/LootDrops/FireRateBuff.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/LootDrops/ModifierDrop.h"
+#include "FireRateBuff.generated.h"
+
+
+UCLASS()
+class TPSTEMPLATE_API AFireRateBuff : public AModifierDrop
+{
+	GENERATED_BODY()
+	
+};
