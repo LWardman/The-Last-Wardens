3f144114bd552853a9cdc7bb1b507bf15170a0a3
Lewis Wardman <lewis2304@hotmail.co.uk>
2024-06-13 15:44:22 +0100

    Filled out based on damage buff logic, since both modifiers affect the weapon and not the player

--- Source/TPSTemplate/Private/Actors/LootDrops/FireRateBuff.cpp
+++ Source/TPSTemplate/Private/Actors/LootDrops/FireRateBuff.cpp
@@ -1,2 +1,31 @@
 #include "Actors/LootDrops/FireRateBuff.h"
 
+#include "Characters/Shooter.h"
+#include "Actors/Weapon.h"
+#include "Components/ModifierComponent.h"
+
+void AFireRateBuff::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AFireRateBuff::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+                        class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                        const FHitResult& SweepResult)
+{
+	Super::OnOverlapBegin(OverlappedComp, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+    if (!OtherActor) return;
+
+    AShooter* Character = Cast<AShooter>(OtherActor);
+    if (!Character) return;
+
+    AWeapon* Weapon = Character->Weapon;
+    if (!Weapon) return;
+
+    if (UModifierComponent* ModComponent = Weapon->FireRateModifierComponent)
+    {
+        ModComponent->ApplyBuff(FireRateMultiplier, BuffDuration);
+        Destroy();
+    }
+}
--- Source/TPSTemplate/Public/Actors/LootDrops/FireRateBuff.h
+++ Source/TPSTemplate/Public/Actors/LootDrops/FireRateBuff.h
@@ -10,4 +10,17 @@ class TPSTEMPLATE_API AFireRateBuff : public AModifierDrop
 {
 	GENERATED_BODY()
 	
+protected:
+
+    virtual void BeginPlay() override;
+
+public:
+
+	virtual void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+                        class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                        const FHitResult& SweepResult) override;
+
+    float FireRateMultiplier = 1.5f;
+
+    float BuffDuration = 30.f;
 };
